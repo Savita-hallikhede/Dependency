3ee24c42650f8efc2e8e1aa413a108cbb4f3bf6e
savita <savitahallikhede11@gmail.com>
2026-04-10 01:49:12 +0530

    Add project files.

---
+++ Controllers/HarshController.cs
@@ -0,0 +1,31 @@
+﻿using InterfaceMethods;
+using MethodImplementation;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Dependency.Controllers
+{
+    public class HarshController : Controller
+    {
+        private readonly StudentInterface _studntInterface;
+
+        //Constructor injection
+        public HarshController(StudentInterface studntInterface)
+        {
+            _studntInterface = studntInterface;
+        }
+   
+              
+        public IActionResult Display()
+        {
+            //StudentImplementation obj = new StudentImplementation();
+
+            //long DATA = obj.Add(10, 20);
+             
+            
+            long DATA = _studntInterface.Add(10, 20);
+            return View(DATA);
+
+        }
+    }
+}
